d1e83ce9b161a9d7a1072f03407939276033a273
D'arcy Stubbins <darcystubbins.dev.99@gmail.com>
2024-07-10 16:42:14 +0100

    first commit

---
+++ php-files/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+/** 
+ * This is where you say:
+ * "www.mycoolsite.com/my/end/point needs to redirect to abc.php"
+ * "www.mycoolsite.com/my/other/end/point needs to redirect to xyz.php"
+ * etc...
+ * 
+ * For now, it just returns php info which tells you everything about your PHP installation like enabled packages, version info, maximum upload size, etc
+*/
+
+phpinfo(); 
+
+?>
